f676eb805cfbb85823dd0354bac1f6b64866404a
JuniorRosario24 <rosariojunior1024@gmail.com>
2025-03-12 23:35:15 +0100

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,4 +1,10 @@
-const saludar = require('../app'); 
-test('Saludar a una persona', () =
-  expect(saludar('Juan')).toBe('Hola, Juan!'); 
-}); 
+// tests/app.test.js
+const saludar = require('../app');  // Aquí se importa app.js desde la carpeta superior
+
+test('saludar con nombre "Juan" debe devolver "Hola, Juan!"', () => {
+  expect(saludar('Juan')).toBe('Hola, Juan!');
+});
+
+test('saludar con nombre "Junior" debe devolver "Hola, Junior!"', () => {
+  expect(saludar('Junior')).toBe('Hola, Junior!');
+});
